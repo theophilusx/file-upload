fccb30236e5c0792f2bc0ba7608bff75ad1333fa
Tim Cross <theophilusx@gmail.com>
2015-05-21 18:57:47 +1000

    Moved to use static index.html instead of dynamic

--- src/clj/file_upload/handler.clj
+++ src/clj/file_upload/handler.clj
@@ -1,5 +1,6 @@
 (ns file-upload.handler
-  (:require [compojure.core :refer [GET POST defroutes]]
+  (:require [clojure.java.io :as io]
+            [compojure.core :refer [GET POST defroutes]]
             [compojure.route :refer [not-found resources]]
             [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
             [ring.middleware.format :refer [wrap-restful-format]]
@@ -10,46 +11,33 @@
             [environ.core :refer [env]]
             [clojure.java.io :as io]))
 
-(def home-page
-  (html
-   [:html
-    [:head
-     [:meta {:charset "utf-8"}]
-     [:meta {:name "viewport"
-             :content "width=device-width, initial-scale=1"}]
-     (include-css (if (env :dev) "css/site.css" "css/site.min.css"))]
-    [:body
-     [:div#app]
-     (include-js "js/app.js")]]))
-
-(defn handle-upload [req]
-  (let [{:keys [filename size tempfile]} (get-in req [:params :upload-file])]
-    (println (str "handle-upload: Filename: " filename " size: " size
-                  " tempfile: " (str tempfile)))
-    (cond
-      (= "" filename) {:status 400
-                       :headers {"Content-Type" "application/json"}
-                       :body (generate-string {:status "ERROR"
-                                               :message "No file parameter sent"})}
-      (< size 100) {:status 400
-                    :headers {"Content-Type" "application/json"}
-                    :body (generate-string {:status "ERROR"
-                                            :message (str "File less than 100 bytes"
-                                                          " - Can't be bothered")})}
-      (>= size 100) {:status 200
+(defn handle-upload [{:keys [filename size tempfile]}]
+  (println (str "handle-upload: Filename: " filename " size: " size
+                " tempfile: " (str tempfile)))
+  (cond
+    (= "" filename) {:status 400
                      :headers {"Content-Type" "application/json"}
-                     :body (generate-string {:status "OK"
-                                             :filename filename
-                                             :size size
-                                             :tempfile (str tempfile)})}
-      :else {:status 400
-             :headers {"Content-Type" "application/json"}
-             :body (generate-string {:status "ERROR"
-                                     :message "Unexpected Error"})})))
+                     :body (generate-string {:status "ERROR"
+                                             :message "No file parameter sent"})}
+    (< size 100) {:status 400
+                  :headers {"Content-Type" "application/json"}
+                  :body (generate-string {:status "ERROR"
+                                          :message (str "File less than 100 bytes"
+                                                        " - Can't be bothered")})}
+    (>= size 100) {:status 200
+                   :headers {"Content-Type" "application/json"}
+                   :body (generate-string {:status "OK"
+                                           :filename filename
+                                           :size size
+                                           :tempfile (str tempfile)})}
+    :else {:status 400
+           :headers {"Content-Type" "application/json"}
+           :body (generate-string {:status "ERROR"
+                                   :message "Unexpected Error"})}))
 
 (defroutes routes
-  (GET "/" [] home-page)
-  (POST "/upload" [upload-file] handle-upload)
+  (GET "/" [] (slurp (io/resource "public/index.html")))
+  (POST "/upload" [upload-file] (handle-upload upload-file))
   (resources "/")
   (not-found "Not Found"))
 
